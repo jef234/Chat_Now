4fdf1f6e0114d338292f43ac2d3c423dc2587bb2
Jeffrey Rajkumar <Jeffrey234@gmail.com>
2019-08-17 09:38:40 +0530

    heroku config5

--- server.js
+++ server.js
@@ -20,8 +20,9 @@ io.on("connection", function (socket) {
 });
 
 // app.use(express.static("public"));
-app.set('view engine', 'ejs')
-app.get('/', (req, res) => res.render(port+'/public/index'))
+// server.set('view engine', 'ejs')
+// server.get('/', (req, res) => res.render('/public/index'))
+app.get('/', (req, res) => res.sendFile(__dirname,'/public/index.html'))
 
 server.listen(PORT, function () {
     console.log("Server started on PORT: " + PORT)
